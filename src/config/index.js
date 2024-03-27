09a76eab9304141520a898c1e619e8ad539b3378
mynorgc79 <mynorguarchaj99@gmail.com>
2024-03-27 15:34:39 -0600

    agregando build

--- src/config/index.js
+++ src/config/index.js
@@ -1,16 +1,16 @@
 const config = {
-  local: {
-    /// local environment
-        API_URL: 'https://api-finalproject-payroll.onrender.com/api/v1',
-  },
-  test: {
-    // test environment
-    API_URL: '',
-  },
-  production: {
-    // production environment
-    API_URL: '', 
-  },
+    local: {
+        /// local environment
+        API_URL: 'http://localhost:8000/api/v1',
+    },
+    test: {
+        // test environment
+        API_URL: '',
+    },
+    production: {
+        // production environment
+        API_URL: '',
+    },
 }
 
 const nodeEnv = import.meta.env.VITE_NODE_ENV || 'local'
